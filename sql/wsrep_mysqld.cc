17bf7f22f34c96aad4575410456d75611d68802f
mkaruza <mario.karuza@galeracluster.com>
2018-12-17 14:38:15 +0100

    Galera cache encryption
    * Setting encryption key to provider
    * Implementation of do_crypt function used for enc / dec
    * Implementation of serialization / deserialization

--- sql/wsrep_mysqld.cc
+++ sql/wsrep_mysqld.cc
@@ -2545,7 +2545,6 @@ bool wsrep_provider_is_SR_capable()
   return Wsrep_server_state::has_capability(wsrep::provider::capability::streaming);
 }
 
-
 int wsrep_ordered_commit_if_no_binlog(THD* thd, bool all)
 {
   if (((wsrep_thd_is_local(thd) &&
@@ -2879,6 +2878,38 @@ enum wsrep::streaming_context::fragment_unit wsrep_fragment_unit(ulong unit)
     DBUG_ASSERT(0);
     return wsrep::streaming_context::bytes;
   }
+}
+
+bool wsrep_set_encryption_key(const void* key, size_t size, unsigned int version)
+{
+    std::vector<unsigned char> input;
+    wsrep_key_serialize(input, key, size, version);
+    return Wsrep_server_state::instance().set_encryption_key(input);
+}
+
+static const unsigned int key_version_preamble_size = 8;
+
+void wsrep_key_serialize(std::vector<unsigned char>& input, const void* key,
+                         size_t size, unsigned int version)
+{
+  input.resize(key_version_preamble_size + size);
+  memcpy(input.data(), (void *)&version, sizeof(version));
+  memcpy(input.data() + key_version_preamble_size, key, size);
+}
+
+bool wsrep_key_deserialize(const void* input, const size_t& input_size,
+                           const void*& key_ptr, size_t& size,
+                           unsigned int& version)
+{
+  bool ret= true;
+  if(input_size >= key_version_preamble_size)
+  {
+    memcpy(&version, input, sizeof(version));
+    key_ptr= input + key_version_preamble_size;
+    size= input_size - key_version_preamble_size;
+    ret= false;
+  }
+  return ret;
 }
 
 /***** callbacks for wsrep service ************/
