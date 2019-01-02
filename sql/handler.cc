a088cba07781a8c2ed86cfa5ee1c78a4980cdb55
sjaakola <seppo.jaakola@iki.fi>
2019-01-02 11:25:54 +0200

    Fixes according to Svoj review #5
    * fixed the logic in detecting partition engine
    * removed duplicate condition in binlog_log_row_internal()
      (same check already in uper level binlog_log_row())

--- sql/handler.cc
+++ sql/handler.cc
@@ -6249,16 +6249,6 @@ static int binlog_log_row_internal(TABLE* table,
   bool error= 0;
   THD *const thd= table->in_use;
 
-#ifdef WITH_WSREP
-  /* only InnoDB tables will be replicated through binlog emulation */
-  if (WSREP_EMULATE_BINLOG(thd) &&
-      table->file->ht->flags & HTON_WSREP_REPLICATION &&
-      !(table->file->ht->db_type == DB_TYPE_PARTITION_DB &&
-        (((ha_partition*)(table->file))->ht->flags && HTON_WSREP_REPLICATION)))
-  {
-      return 0;
-  }
-#endif /* WITH_WSREP */
   /*
     If there are no table maps written to the binary log, this is
     the first row handled in this statement. In that case, we need
@@ -6289,10 +6279,13 @@ int binlog_log_row(TABLE* table, const uchar *before_record,
   THD *const thd= table->in_use;
 
   /* only InnoDB tables will be replicated through binlog emulation */
-  if ((WSREP_EMULATE_BINLOG(thd) &&
-       table->file->partition_ht()->flags & HTON_WSREP_REPLICATION) ||
-       (thd->wsrep_ignore_table == true))
-    return 0;
+  if (WSREP_EMULATE_BINLOG(thd) &&
+      !(table->file->ht->flags & HTON_WSREP_REPLICATION) &&
+      !(table->file->ht->db_type == DB_TYPE_PARTITION_DB &&
+        (((ha_partition*)(table->file))->ht->flags & HTON_WSREP_REPLICATION)))
+  {
+      return 0;
+  }
 #endif
 
   if (!table->file->check_table_binlog_row_based(1))
