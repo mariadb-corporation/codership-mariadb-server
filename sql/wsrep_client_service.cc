3c2f8edc77201497c708ef66633dc15682a42d94
sjaakola <seppo.jaakola@iki.fi>
2023-05-22 11:49:11 +0300

    Skipping failure for empty RBR buffer for SR transactions, which may have empty last WS carrying only commit flag

--- sql/wsrep_client_service.cc
+++ sql/wsrep_client_service.cc
@@ -136,7 +136,10 @@ int Wsrep_client_service::prepare_data_for_replication()
     {
       WSREP_DEBUG("empty rbr buffer, query: %s", wsrep_thd_query(m_thd));
     }
-    DBUG_RETURN(1);
+
+    /* SR may have empty last WS to just carry the comit flag */
+    if (!m_thd->wsrep_trx().is_streaming())
+      DBUG_RETURN(1);
   }
   DBUG_RETURN(0);
 }
